de6c07ddb64c1c5f98138a9d9153f91844d07ff4
Junbang <biatbang@163.com>
2020-03-07 16:06:07 -0500

    add candidates and client side

--- contracts/Election.sol
+++ contracts/Election.sol
@@ -4,12 +4,63 @@ contract Election{
     // store the candidates
     // provide reading and writing
 
-    string public candidate;
+    // string public candidate;
 
-    // constructor
-    constructor() public {
-        // underline means state variable
-        candidate = "cand1";
+    // // constructor
+    // constructor() public {
+    //     // underline means state variable
+    //     candidate = "cand1";
+    // }
+
+    // model a candidate
+    struct Candidate {
+        // unsigned integer
+        uint id;
+        string name;
+        uint voteCount;
     }
 
+    // key => value to store the candidates
+    mapping (uint => Candidate) public candidates;
+
+    // Store accounts that have voted
+    mapping (address => bool) public voters;
+
+    // to count the number, since mapping has no function of counting
+    // brutely
+    uint public candidatesCount;
+
+    // constraint the range of candidates
+    constructor() public {
+        addCandidate("cand 1");
+        addCandidate("cand 2");
+    }
+
+    // here memory is mandatory to tell the location of the parameter
+    function addCandidate(string memory _name) private {
+        candidatesCount ++;
+        candidates[candidatesCount] = Candidate(candidatesCount, _name, 0);
+    }
+
+    // voted event
+    event votedEvent (
+        uint indexed _candidateId
+    );
+
+    function vote (uint _candidateId) public {
+        // require that they haven't voted before
+        require(!voters[msg.sender]);
+
+        // require a valid candidate
+        require(_candidateId > 0 && _candidateId <= candidatesCount);
+
+        // record that voter has voted
+        voters[msg.sender] = true;
+
+        // update candidate vote Count
+        candidates[_candidateId].voteCount ++;
+
+        // trigger voted event
+        emit votedEvent(_candidateId);
+    }
 }
---
+++ test/election.js
@@ -0,0 +1,13 @@
+var Election =  artifacts.require('./Election.sol');
+
+contract("Election", accounts => {
+    it("init with 2 cands", () => {
+        return Election.deployed().then(i => {
+            return i.candidatesCount();
+        }).then(count => {
+            assert.equal(count, 2);
+        })
+    });
+
+    // 52:48 another test
+})
